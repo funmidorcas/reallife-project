11a8aa977a2726da2514cf6d4555e917c0cd5007
funmidorcas <oladipupofunmilayodorcas@gmail.com>
2025-11-15 23:43:32 +0100

    toggle

--- Task work/dashboard.js
+++ Task work/dashboard.js
@@ -1,1 +1,10 @@
 
+const header = document.querySelector(".header")
+const main = document.querySelector("main")
+window.addEventListener("load", ()=>{
+    main.classList.add("header-click")
+console.log("it worked")
+})
+header.addEventListener("click", ()=>{
+    main.classList.toggle("header-click")
+})
